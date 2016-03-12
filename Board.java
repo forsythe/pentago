2474f6a3a7668a6ca6993e6a6f6fff5f3c9be225
forsythe <fjorsyth@gmail.com>
2016-03-12 14:15:58 +0800

    added printing board

--- Board.java
+++ Board.java
@@ -1,5 +1,67 @@
 package pentago;
 
 public class Board {
-	long[] board = new long[2]; //0 for white, 1 for black
+	private static final int WHITE = 0, BLACK = 1;
+
+	long[] board = new long[2]; // 0 for white, 1 for black
+
+	public void print(int color) {
+		System.out.println((color == 0 ? "white" : "black"));
+
+		String temp = getStringFromLong(this.board[color]);
+		assert temp.length() == 36;
+
+		System.out.println(temp);
+		for (int k = 0; k < 36; k++) {
+			System.out.print(temp.charAt(k));
+			if ((k + 1) % 3 == 0)
+				System.out.print("|");
+			if ((k + 1) % 6 == 0)
+				System.out.println();
+			if (k == 17)
+				System.out.println("---+----");
+		}
+		System.out.println();
+	}
+
+	public void print() {
+		String temp = "";
+		for (int k = 35; k >= 0; k--) {
+			if (getCell(WHITE, k) == getCell(BLACK, k)) {
+				temp += "O";
+			} else if (getCell(WHITE, k) == 1) {
+				temp += "W";
+			} else {
+				temp += "B";
+			}
+		}
+		assert temp.length() == 36;
+
+		for (int k = 0; k < 36; k++) {
+			System.out.print(temp.charAt(k));
+			if ((k + 1) % 3 == 0)
+				System.out.print("|");
+			if ((k + 1) % 6 == 0)
+				System.out.println();
+			if (k == 17)
+				System.out.println("---+----");
+		}
+		System.out.println();
+	}
+
+	private void setCell(Board b, int color, int pos) {
+		b.board[color] |= (1L << pos);
+		System.out.println("Adding marble to " + (color == 0 ? "white" : "black") + "'s position " + pos);
+	}
+
+	private long getCell(int color, int pos) {
+		return (this.board[color] & (1L << pos)) >> pos;
+
+	}
+
+	private static String getStringFromLong(Long b) {
+		String temp = Long.toBinaryString(b);
+		return String.format("%36s", temp).replace(' ', '0');
+		// beware, leading 0s lost when Long-->str, so pad them back
+	}
 }
--- PentagoAI.java
+++ PentagoAI.java
@@ -2,27 +2,17 @@ package pentago;
 
 public class PentagoAI {
 
+	public static final int WHITE = 0, BLACK = 1;
+
 	public static void main(String[] args) {
 		Board b = new Board();
-		b.board[0] = 0b100001000000000000000000000000100001L;
-		printBoard(b, 0);
-		//setCell(b, 0, 4);
-		//printBoard(b, 0);
+		//b.board[0] = 0b100001000000000000000000000000100001L;
+		//b.board[1] = 0b010010000000000000000000000000010010L;
+		//b.print(WHITE);
+		//b.print(BLACK);
+		b.print();
+				
+
 	}
-	
-	private static void printBoard(Board b, int color){
-		
-		String temp = Long.toBinaryString(b.board[color]);
-		for (int k = 0; k < 36; k++){
-			System.out.print(temp.charAt(k));
-			if (k%3==0) System.out.print("|");
-			if (k%6==0) System.out.println();
-			if (k==18) System.out.println("---+----");
-		}
-		System.out.println();
-	}
-	
-	private static void setCell(Board b, int color, int pos){
-		b.board[color] |= (1L<<pos);
-	}
+
 }
