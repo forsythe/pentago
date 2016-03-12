15d3719b015323ff61908e3d3430297ba00d0528
forsythe <fjorsyth@gmail.com>
2016-03-12 18:23:34 +0800

    finished rotateQuadrant function

--- Board.java
+++ Board.java
@@ -5,10 +5,15 @@ public class Board {
 
 	long[] board = new long[2]; // 0 for white, 1 for black
 
+	/*
+	 * Board representation: [35, 34, 33, .... 2, 1, 0] 35 | 34 | 33 ||| 32 | 31 | 30 29 | 28 | 27 ||| 26 | 25 | 24 23 | 22 | 21 ||| 20 | 19 | 18
+	 * ==============+============== 17 | 16 | 15 ||| 14 | 13 | 12 11 | 10 | 9 ||| 8 | 7 | 6 5 | 4 | 3 ||| 2 | 1 | 0
+	 */
+
 	public void print(int color) {
 		System.out.println((color == 0 ? "white" : "black"));
 
-		String temp = getStringFromLong(this.board[color]);
+		String temp = getBinaryStringFromLong(this.board[color]);
 		assert temp.length() == 36;
 
 		System.out.println(temp);
@@ -36,8 +41,8 @@ public class Board {
 				temp += "B";
 			}
 		}
-		System.out.println("white: " + getStringFromLong(board[WHITE]));
-		System.out.println("black: " + getStringFromLong(board[BLACK]));
+		System.out.println("white: " + getBinaryStringFromLong(board[WHITE]));
+		System.out.println("black: " + getBinaryStringFromLong(board[BLACK]));
 		for (int k = 0; k < 36; k++) {
 			System.out.print(temp.charAt(k));
 			if ((k + 1) % 3 == 0)
@@ -50,7 +55,7 @@ public class Board {
 		System.out.println();
 	}
 
-	private static String getStringFromLong(Long b) {
+	private static String getBinaryStringFromLong(Long b) {
 		String temp = Long.toBinaryString(b);
 		return String.format("%36s", temp).replace(' ', '0');
 		// beware, leading 0s lost when Long-->str, so pad them back
@@ -69,7 +74,7 @@ public class Board {
 
 	// Sets the cell value to something of our choice
 	private void setCell(int color, int pos, long value) {
-		//System.out.println("Setting playerboard " + color + "'s bit position: " + pos + " to the value " + value);
+		// System.out.println("Setting playerboard " + color + "'s bit position: " + pos + " to the value " + value);
 		board[color] ^= (-value ^ board[color]) & (1L << pos); // Relies on twos complement for long OTHERWISE GG
 	}
 
@@ -133,12 +138,65 @@ public class Board {
 					setCell(k, 2, temp);
 				}
 				break;
-
 			}
 
 		} else {
 			switch (quadrant) {
 			case 1:
+				for (int k = WHITE; k <= BLACK; k++) {
+					temp = getCell(k, 24);
+					setCell(k, 24, getCell(k, 19));
+					setCell(k, 19, getCell(k, 26));
+					setCell(k, 26, getCell(k, 31));
+					setCell(k, 31, temp);
+					temp = getCell(k, 18);
+					setCell(k, 18, getCell(k, 20));
+					setCell(k, 20, getCell(k, 32));
+					setCell(k, 32, getCell(k, 30));
+					setCell(k, 30, temp);
+				}
+				break;
+			case 2:
+				for (int k = WHITE; k <= BLACK; k++) {
+					temp = getCell(k, 27);
+					setCell(k, 27, getCell(k, 22));
+					setCell(k, 22, getCell(k, 29));
+					setCell(k, 29, getCell(k, 34));
+					setCell(k, 34, temp);
+					temp = getCell(k, 21);
+					setCell(k, 21, getCell(k, 23));
+					setCell(k, 23, getCell(k, 35));
+					setCell(k, 35, getCell(k, 33));
+					setCell(k, 33, temp);
+				}
+				break;
+			case 3:
+				for (int k = WHITE; k <= BLACK; k++) {
+					temp = getCell(k, 9);
+					setCell(k, 9, getCell(k, 4));
+					setCell(k, 4, getCell(k, 11));
+					setCell(k, 11, getCell(k, 16));
+					setCell(k, 16, temp);
+					temp = getCell(k, 3);
+					setCell(k, 3, getCell(k, 5));
+					setCell(k, 5, getCell(k, 17));
+					setCell(k, 17, getCell(k, 15));
+					setCell(k, 15, temp);
+				}
+				break;
+			case 4:
+				for (int k = WHITE; k <= BLACK; k++) {
+					temp = getCell(k, 6);
+					setCell(k, 6, getCell(k, 1));
+					setCell(k, 1, getCell(k, 8));
+					setCell(k, 8, getCell(k, 13));
+					setCell(k, 13, temp);
+					temp = getCell(k, 0);
+					setCell(k, 0, getCell(k, 2));
+					setCell(k, 2, getCell(k, 14));
+					setCell(k, 14, getCell(k, 12));
+					setCell(k, 12, temp);
+				}
 				break;
 			}
 		}
